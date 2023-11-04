8ced6dcd7a0a641b3830fd6bdb8cd4c9ec2c2e79
devsetgo <mikeryan56@gmail.com>
2023-11-04 20:16:53 +0000

    making a change to improve loop test

--- loop.py
+++ loop.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import asyncio
 import secrets
 
+
 CONCURRENT_REQUEST_LIMIT = 2000  # Limit to prevent PoolTimeout, adjust as needed
 REQUEST_TIMEOUT = 10.0  # Time to wait for server to respond, adjust as needed
 
@@ -16,10 +17,11 @@ async def run(n):
     timeout = Timeout(REQUEST_TIMEOUT)  # Set request timeout
     async with AsyncClient(timeout=timeout) as client:
         tasks = []
-        for i in tqdm(range(n)):
+        for i in tqdm(range(n), ascii=True):
             data = {"name": f"user-{i}-{secrets.token_hex(4)}"}
             task = send_request(
-                client, semaphore, "http://localhost:5000/users/", json=data
+                client, semaphore, "http://localhost:5000/users"
+                # , json=data
             )
             tasks.append(task)
 
@@ -30,19 +32,33 @@ async def run(n):
 
 async def send_request(client, semaphore, *args, **kwargs):
     async with semaphore:  # Only allow a certain number of requests to be sent at once
-        return await client.post(*args, **kwargs)
+        return await client.get(*args, **kwargs)
 
 
 if __name__ == "__main__":
-    n = 1000  # replace with the number of posts you want to make
-    start_time = time.time()
-    responses = asyncio.run(run(n))
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    requests_per_second = n / elapsed_time
-    print(
-        f"Elapsed time: {format(elapsed_time, '.2f')} seconds"
-    )  # Limit to 2 decimal places
-    print(
-        f"Requests per second: {format(requests_per_second, '.2f')}"
-    )  # Limit to 2 decimal places
+    n = 10000  # replace with the number of posts you want to make
+    loops = 10
+    req_list = []
+    t0 = time.time()
+    for l in range(loops):
+
+        start_time = time.time()
+        responses = asyncio.run(run(n))
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        requests_per_second = n / elapsed_time
+
+        req_list.append(requests_per_second)
+        # print(
+        #     f"Elapsed time: {format(elapsed_time, '.2f')} seconds"
+        # )  # Limit to 2 decimal places
+        # print(
+        #     f"Requests per second: {format(requests_per_second, '.2f')}"
+        # )  # Limit to 2 decimal places
+
+    from statistics import mean, median
+    ave = mean(req_list)
+    med = median(req_list)
+    t1 = time.time() - t0
+    total_req = n*loops
+    print(F"Mean: {ave:.2f}, Median: {med:.2f}, TotalRequests:{n*loops} in {t1:.2f} seconds, Process Mean Reqests: {total_req/t1:.2f}")
